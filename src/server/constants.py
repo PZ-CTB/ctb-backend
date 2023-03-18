e15536d2171b94bc72e4a316a67facde3dc3f52f
Paweł Bielecki <pawelek24@op.pl>
2023-03-18 21:48:26 +0100

    Poprawki: przywrócenie częsci ustawień środowiska i dokumentacja klasy Paths

--- src/server/constants.py
+++ src/server/constants.py
@@ -1,15 +1,10 @@
 import os
 
-def defaultEnv(key: str, default: str = ""):
-    value = os.getenv(key)
-    if value is None:
-        return default
-    return value
-
 class PATHS:
+    """Paths (constants) to any resources in the project."""
 
-    APPLICATION_ROOT_PATH = defaultEnv("APPLICATION_ROOT_PATH", "./")
-    VAR_PATH = defaultEnv("VAR_PATH", "/tmp/")
+    APPLICATION_ROOT_PATH = os.getenv("APPLICATION_ROOT_PATH", "./")
+    VAR_PATH = os.getenv("VAR_PATH", "/tmp/")
     
     RESOURCES = APPLICATION_ROOT_PATH + "res/"
     DATASETS = RESOURCES + "datasets/"
@@ -18,7 +13,5 @@ class PATHS:
     DATABASE_DEFAULT_DUMP = RESOURCES + "dump.sql"
     DATABASE = VAR_PATH + "database_ctb.db"
     
-    CERTIFICATES_PATH = RESOURCES + "cert/"
-    CERTIFICATE = CERTIFICATES_PATH + "example.crt"
-    CERTIFICATE_KEY = CERTIFICATES_PATH + "example.key"
+
 
