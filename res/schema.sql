72f821641ee5cb5563f3760e45c3c2323c9962a4
rafalw99 <127767606+rafalw99@users.noreply.github.com>
2023-03-21 21:25:37 +0100

    [CTB-68] Implement basic authentication system (#6)
    * [CTB-68] Implement basic authentication system
    
    Added endpoints:
    - register
    - login
    - me
    - logout
    
    * [CTB-68] Implement basic authentication system
    
    * [CTB-68] Implement basic authentication system
    
    Changes:
    - Improved the error handling
    - Added type annotations
    - Improved the overall code quality
    - Minor database changes
    
    * [CTB-68]  Implement basic authentication system
    
    Minor file format fixes
    
    * [CTB-68]  Implement basic authentication system
    
    Changed regular strings to f-strings
    
    * [CTB-68] - Implement basic authentication system
    
    Minor code quality changes
    
    * [CTB-68] - Implement basic authentication system
    
    revoke_token -> return statement fix
    
    * [CTB-68] - Implement basic authentication system
    
    Endpoint "/refresh" had been implemented.
    
    * [CTB-68] Implement basic authentication system
    
    Changes requested in PR.
    
    * [CTB-68] Implement basic authentication system
    
    Removed unused json import.

--- res/schema.sql
+++ res/schema.sql
@@ -1,13 +1,19 @@
-
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
+    uuid TEXT PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
     password_hash TEXT,
     wallet_usd FLOAT NOT NULL DEFAULT 0.0,
     wallet_btc FLOAT NOT NULL DEFAULT 0.0
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS user_name_index ON users (username);
+CREATE UNIQUE INDEX IF NOT EXISTS user_uuid_index ON users (uuid);
+
+CREATE TABLE IF NOT EXISTS revoked_tokens (
+    token TEXT PRIMARY KEY,
+    expiry TEXT NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS token_index ON revoked_tokens (token);
 
 CREATE TABLE IF NOT EXISTS exchange_rate_history (
     date TEXT PRIMARY KEY CHECK(DATE(STRFTIME("%s", date), "unixepoch") == date),
--- src/server/main.py
+++ src/server/main.py
@@ -1,30 +1,329 @@
-from flask import Flask
+import uuid
+from datetime import datetime, timedelta
+from functools import wraps
+from sqlite3 import Connection, Cursor
+from sqlite3 import Error as SqlError
+from typing import Any, Callable
+from uuid import UUID
+
+import jwt
+from flask import Flask, Response, make_response, request
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from .database_provider import getDatabaseConnection
 
 app = Flask(__name__)
+# Generetion method -> uuid.uuid4().hex
+app.config["SECRET_KEY"] = "55cfba6d5bd6405c8e9b7b681f6b8835"
 
 
-@app.route("/me")
-def me() -> str:
-    """User's information endpoint."""
-    return "me"
+def token_required(fun: Callable[..., Response]) -> Callable[..., Response]:
+    """Validate received token."""
+
+    @wraps(fun)
+    def decorated(*args: tuple, **kwargs: dict) -> Response:
+        """Validate received token."""
+        token: str = ""
+        # JWT is passed in the request header
+        if "x-access-token" in request.headers:
+            token = request.headers["x-access-token"]
+
+        # If the token is not passed return message and exit function
+        if not token:
+            return make_response(
+                {"message": "Token is missing"},
+                401,
+                {"WWW-Authenticate": 'Basic realm ="Token is missing!"'},
+            )
+
+        # If the token is revoked return message and exit function
+        if is_token_revoked(token):
+            return make_response(
+                {"message": "Token is revoked"},
+                401,
+                {"WWW-Authenticate": 'Basic realm ="Token is revoked!"'},
+            )
+
+        # Decode the token and retrieve the information contained in it
+        try:
+            data: dict[str, Any] = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
+        except jwt.InvalidTokenError as e:
+            return make_response(
+                {"message": f"Token is invalid: {e}"},
+                401,
+                {"WWW-Authenticate": 'Basic realm ="Token is invalid!"'},
+            )
+
+        try:
+            connection: Connection = getDatabaseConnection()
+            sql: str = "SELECT uuid, email FROM users WHERE uuid=?"
+            cursor: Cursor = connection.cursor()
+            cursor.execute(sql, (data["uuid"],))
+            user_information: list[tuple[str, str]] = cursor.fetchall()
+            connection.close()
+        except SqlError as e:
+            return make_response({"message": f"Database connection error: {e}"}, 500)
+
+        if not user_information:
+            return make_response(
+                {"message": "User does not exist"},
+                401,
+                {"WWW-Authenticate": 'Basic realm ="User does not exist!"'},
+            )
+
+        # Retrieve user uuid
+        unique_id: str = user_information[0][0]
+
+        # Returns the current logged-in users context to the routes
+        return fun(unique_id, *args, **kwargs)
+
+    return decorated
 
 
-@app.route("/login")
-def login() -> str:
-    """Login endpoint."""
-    return "login"
+def revoke_token(token: str) -> None:
+    """Revoking activated token."""
+    decoded_token: dict[str, Any] = jwt.decode(
+        token, app.config["SECRET_KEY"], algorithms=["HS256"]
+    )
+    expiry: datetime = datetime.fromtimestamp(decoded_token["exp"])
+
+    # Saving revoked token into the database
+    try:
+        connection: Connection = getDatabaseConnection()
+        connection.execute(
+            "INSERT INTO revoked_tokens (token, expiry) VALUES (?, ?)",
+            (
+                token,
+                expiry,
+            ),
+        )
+        connection.commit()
+        connection.close()
+    except SqlError as e:
+        raise SqlError(f"Database connection error: {str(e)}") from e
 
 
-@app.route("/logout")
-def logout() -> str:
-    """Logout endpoint."""
-    return "logout"
+def is_token_revoked(token: str) -> bool:
+    """Check if token is revoked."""
+    try:
+        connection: Connection = getDatabaseConnection()
+        result: Cursor = connection.execute(
+            "SELECT token FROM revoked_tokens WHERE token=? AND expiry > ?",
+            (
+                token,
+                datetime.utcnow(),
+            ),
+        )
+        is_revoked: bool = result.fetchone() is not None
+        connection.close()
+    except SqlError:
+        return True
+
+    return is_revoked
 
 
-@app.route("/register")
-def register() -> str:
+@app.route("/register", methods=["POST"])
+def register() -> Response:
     """Registration endpoint."""
-    return "register"
+    new_user: dict[str, str] = request.json
+
+    unique_id: UUID = uuid.uuid4()
+    email: str = new_user.get("email", "")
+    password: str = new_user.get("password", "")
+
+    if not email or not password:
+        return make_response({"message": "Invalid Json format"}, 202)
+
+    try:
+        connection: Connection = getDatabaseConnection()
+        sql: str = "SELECT * FROM users WHERE email=?"
+        cursor: Cursor = connection.cursor()
+        cursor.execute(sql, (email,))
+        user_information: list[tuple] = cursor.fetchall()
+        connection.close()
+    except SqlError as e:
+        return make_response({"message": f"Database connection error: {e}"}, 501)
+
+    if user_information:
+        return make_response(
+            {"message": "User already exists. Please Log in."},
+            202,
+            {"WWW-Authenticate": "User already exists. Please Log in."},
+        )
+
+    try:
+        # Already defined: connection, sql, cursor
+        connection = getDatabaseConnection()
+        sql = "INSERT INTO users(uuid, email, password_hash) VALUES (?, ?, ?)"
+        cursor = connection.cursor()
+        cursor.execute(
+            sql,
+            (
+                str(unique_id),
+                email,
+                generate_password_hash(password),
+            ),
+        )
+        connection.commit()
+        connection.close()
+    except SqlError as e:
+        return make_response({"message": f"Database connection error: {e}"}, 501)
+
+    return make_response({"message": "Successfully registered"}, 201)
+
+
+@app.route("/login", methods=["POST"])
+def login() -> Response:
+    """Login endpoint."""
+    auth: dict[str, Any] = request.json
+
+    email: str = auth.get("email", "")
+    password: str = auth.get("password", "")
+
+    if not email or not password:
+        return make_response({"message": "Invalid Json format"}, 202)
+
+    try:
+        connection: Connection = getDatabaseConnection()
+        sql: str = "SELECT uuid, email, password_hash FROM users WHERE email=?"
+        cursor: Cursor = connection.cursor()
+        cursor.execute(sql, (email,))
+        user_information: list[tuple[str, str, str]] = cursor.fetchall()
+        connection.close()
+    except SqlError as e:
+        return make_response({"message": f"Database connection error: {e}"}, 501)
+
+    if not user_information:
+        return make_response(
+            {"message": "User does not exist"},
+            401,
+            {"WWW-Authenticate": 'Basic realm ="User does not exist!"'},
+        )
+
+    user_uuid: str = user_information[0][0]  # uuid
+    user_email: str = user_information[0][1]  # email
+    user_password: str = user_information[0][2]  # password
+
+    if check_password_hash(user_password, password):
+        token: str = jwt.encode(
+            {
+                "uuid": user_uuid,
+                "email": user_email,
+                "exp": datetime.utcnow() + timedelta(minutes=30),
+            },
+            app.config["SECRET_KEY"],
+        )
+
+        return make_response({"AUTH-TOKEN": token}, 201)
+
+    return make_response(
+        {"message": "Could not verify"},
+        403,
+        {"WWW-Authenticate": 'Basic realm ="Wrong password"'},
+    )
+
+
+@app.route("/me", methods=["GET"])
+@token_required
+def me(unique_id: str) -> Response:
+    """User's information endpoint."""
+    try:
+        connection: Connection = getDatabaseConnection()
+        sql = "SELECT email, wallet_usd, wallet_btc FROM users WHERE uuid=?"
+        cursor: Cursor = connection.cursor()
+        cursor.execute(sql, (unique_id,))
+        user_information: list = cursor.fetchall()
+        connection.close()
+    except SqlError as e:
+        return make_response({"message": f"Database connection error: {e}"}, 501)
+
+    if not user_information:
+        return make_response(
+            {"message": "User does not exist"},
+            401,
+            {"WWW-Authenticate": 'Basic realm ="User does not exist!"'},
+        )
+
+    email: str = user_information[0][0]
+    wallet_usd: float = user_information[0][1]
+    wallet_btc: float = user_information[0][2]
+
+    return make_response(
+        {"uuid": unique_id, "email": email, "wallet_usd": wallet_usd, "wallet_btc": wallet_btc},
+        200,
+    )
+
+
+@app.route("/logout", methods=["POST"])
+@token_required
+def logout(_unique_id: str) -> Response:
+    """Logout endpoint."""
+    token: str = request.headers["x-access-token"]
+
+    if not token:
+        return make_response(
+            {"message": "Token not delivered"},
+            401,
+            {"WWW-Authenticate": 'Basic realm ="Token not delivered!"'},
+        )
+
+    if is_token_revoked(token):
+        return make_response(
+            {"message": "Token already revoked"},
+            401,
+            {"WWW-Authenticate": 'Basic realm ="Token already revoked!"'},
+        )
+
+    try:
+        revoke_token(token)
+    except SqlError as e:
+        return make_response({"message": f"Database connection error: {e}"}, 501)
+
+    return make_response({"message": "User is successfully logged out"}, 201)
+
+
+@app.route("/refresh", methods=["POST"])
+@token_required
+def refresh(unique_id: str) -> Response:
+    """Token refresh endpoint."""
+    try:
+        connection: Connection = getDatabaseConnection()
+        sql: str = "SELECT email FROM users WHERE uuid=?"
+        cursor: Cursor = connection.cursor()
+        cursor.execute(sql, (unique_id,))
+        user_information: list[tuple[str, str]] = cursor.fetchall()
+        connection.close()
+    except SqlError as e:
+        return make_response({"message": f"Database connection error: {e}"}, 500)
+
+    user_email: str = ""
+
+    if not user_information:
+        return make_response(
+            {"message": "User does not exist"},
+            401,
+            {"WWW-Authenticate": 'Basic realm ="User does not exist!"'},
+        )
+
+    user_email = user_information[0][0]
+
+    new_token: str = jwt.encode(
+        {
+            "uuid": unique_id,
+            "email": user_email,
+            "exp": datetime.utcnow() + timedelta(minutes=30),
+        },
+        app.config["SECRET_KEY"],
+    )
+
+    # Token exists -> checked by decorator
+    token: str = request.headers["x-access-token"]
+    try:
+        revoke_token(token)
+    except SqlError as e:
+        return make_response({"message": f"Database connection error: {e}"}, 501)
+
+    return make_response({"AUTH-TOKEN": new_token}, 201)
 
 
 @app.route("/chart")
