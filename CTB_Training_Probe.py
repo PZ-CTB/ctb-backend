b1ae362945e1bee2852fce990ee0009f4ae46b0d
mik-sondej <57150472+mik-sondej@users.noreply.github.com>
2023-03-25 00:27:37 +0100

    db integration

--- CTB_Training_Probe.py
+++ CTB_Training_Probe.py
@@ -3,94 +3,144 @@ import math
 import numpy as np
 import pandas as pd
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
-#function 'simulating' data from API
-#all its instances should be replaced with API readings
-def avg_value(a, b, c, d):
-  return (a + b + c + d)/4
+from database_provider import getDatabaseConnection
 
-df = pd.read_csv("coin_Bitcoin.csv")  # reading csv file
+#CONST
+NO_VALUES = 3
 
-# data set consists of 4 values: High, Low, Open, Close
-numOfValues = 4
-age = 0  # how many times same timeJump has been used for this vector
-counter = 0  # how many days has been used in this data set
-loop = 0  # how many data sets were already put in this vector
-rows = 0  # which vector is being prepared right now
-timeJump = np.array([1, 3, 7, 14, 30])  # how many days will be used for this data set in the vector
-tjIndex = 0  # used to mark which timeJump is being used
-probeDist = 1 # distance in days between starting of each data set
-tempData = np.zeros((numOfValues))
+def _get_data():
+  connection = getDatabaseConnection()
+  cursor = connection.cursor()
+  sql_data = """
+  SELECT
+    date,
+    value
+  FROM
+    exchange_rate_history
+  GROUP BY date;
+             """
+  cursor.execute(sql_data)
+  data = cursor.fetchall()
+  cursor.close()
+
+  return data
+
+def _for_date(date, data):
+  for i in range(round(len(data)/2)):
+    if date == datetime.strptime(data[i][0], '%Y-%m-%d'):
+      return i
+  return -1
+
+def _get_index_start_end(dateStart, dateEnd, data):
+  indexStart = _for_date(dateStart, data)
+  indexEnd =_for_date(dateEnd, data)
+  return indexStart, indexEnd
+
+def _correct_date(dateStart, dateEnd):
+  if (dateStart > dateEnd):
+    return False
+  return True
+
+def _get_no_days(dateStart, dateEnd):
+  return (dateEnd - dateStart).days + 1
+
+def _get_vector_length(numOfDays, ageLimit):
+  length = 0
+  age = 0
+  lengthInDays = 1
+  while(numOfDays > 0):
+    length += 1
+    age += 1
+    numOfDays -= lengthInDays
+    if (age%10 == 0):
+      lengthInDays += 1
+  return length * NO_VALUES + 1
+
+def get_vector_columns(dateStart, dateEnd, ageLimit = 10):
+  return _get_vector_length(_get_no_days(dateStart, dateEnd), ageLimit)
+
+#dateStart, dataEnd = datetime(year, month, day)
+def get_vector(dateStart, dateEnd, ageLimit = 10):
+  if (not _correct_date(dateStart, dateEnd)):
+    return None
+
+  data = _get_data()
+  noDays = _get_no_days(dateStart, dateEnd)
+  
+  timeJump = 1  # how many days will be used for this data set in the vector
+  # data set consists of 3 values: High, Avg, Low
+  age = 0  # how many times same timeJump has been used for this vector
+  counter = 0 # how many days has been used in this data set
+  dataSetCounter = 0  # how many data sets have been prepared
+  tempData = np.zeros((NO_VALUES))
+  length = get_vector_columns(dateStart, dateEnd, ageLimit)
+
+  vector = np.zeros(length)  # input data
+  indexStart, indexEnd = _get_index_start_end(dateStart, dateEnd, data)
+  y_value = data[indexStart - 1][1]  # output data
+
+  # last value of each vector is a month (starting time)
+  vector[length - 1] = datetime.strptime(data[indexEnd][0], '%Y-%m-%d').month
+
+  # max number of vectors that can be achieved with set parameters from data file
+  for i in range(noDays):
+    if counter == 0:
+    # values as in data file
+      tempData[0] = data[i][1]  # High
+      tempData[1] = data[i][1]  # Avg
+      tempData[2] = data[i][1]  # Low
+    else:
+      # values of multiple days streamlined into one
+      tempData[0] = max(data[i][1], tempData[0])  # High
+      tempData[1] = (data[i][1]+tempData[1])/2  # Avg
+      tempData[2] = min(data[i][1], tempData[2])  # Low
+
+    # after performing above operations day is added to the count
+    counter += 1
+
+    # if data set is fully realised (enough days worth of data was compiled into it),
+    # fill vector with it
+    if (counter == timeJump):
+      for k in range(dataSetCounter * NO_VALUES, dataSetCounter * NO_VALUES + NO_VALUES):
+        vector[k] = tempData[k % NO_VALUES]
+      dataSetCounter += 1
+      counter = 0
+      # after injecting data into vector, add to the count of data sets with the same
+      # timeJump in this vector
+      age += 1
+      # if enough same days length data sets has been used, expand the scope (move timeJump
+      # by 1 step)
+      if age == ageLimit:
+          age = 0
+          timeJump += 1
+  for k in range(dataSetCounter * NO_VALUES, dataSetCounter * NO_VALUES + NO_VALUES):
+    vector[k] = tempData[k % NO_VALUES]
+  return vector, y_value
+
+
+#####input i output
+def get_vectorS(dateStart, dateEnd, noRows, ageLimit = 10):
+  length = get_vector_columns(dateStart, dateEnd, ageLimit)
+  vectors = np.zeros((noRows, length))
+  y_values = np.zeros((noRows, length))
+  for i in range(noRows):
+    timeshift = timedelta(days = i)
+    vectors[i], y_values[i] = get_vector(dateStart + timeshift, dateEnd + timeshift)
+  return vectors, y_values
 
 
 
-# size is calculated based on size of data from file, length (in days) of one vector and difference
-# between neighbouring vector starting (ending) time
-vectors = np.zeros((math.floor((df.Date.size - 372) / probeDist) + 1, 40 * numOfValues - 10 * (numOfValues - 1) + 1))  # input data
-y_values = np.zeros((math.floor((df.Date.size - 372) / probeDist) + 1, 1))  # output data
-# 40 - number of max loops in vector, 4 - number of values
+#!!!!!!!!!!!!!!
+#EXAMPLE OF USE
+#dateStart = datetime(2014, 1, 1)
+#dateEnd = datetime(2015, 1, 1)
+#vector, y_value = get_vector(dateStart, dateEnd) #single vector
 
+#length = get_vector_columns(dateStart, dateEnd) #returns you no of collumns target vector should have to accomodate all the data
+#noRows = 8
+#vectors = np.zeros((noRows, length))
+#y_values = np.zeros((noRows, length))
 
-# fills output with corresponding day values
-for g in range (0, y_values.size):
-    y_values[g] = avg_value(df.High[df.Date.size - 1 - g*probeDist], df.Low[df.Date.size - 1 - g*probeDist], df.Open[df.Date.size - 1 - g*probeDist], df.Close[df.Date.size - 1 - g*probeDist])
-
-
-# max number of vectors that can be achieved with set parameters from data file
-while df.Date.size - 371 - rows * probeDist > 0:
-    # last value of each vector is a month (starting time)
-    date = datetime.strptime(df.Date[df.Date.size-1 - rows*probeDist], '%Y-%m-%d %H:%M:%S')
-    vectors[rows][vectors.shape[1] - 1] = date.month
-
-    # fills first 10 days, as they do not have different high, low, open and close values
-    for l in range (0, 10):
-            vectors[rows][l] = avg_value(df.High[df.Date.size - 2 - rows * probeDist - l], df.Low[df.Date.size - 2 - rows * probeDist - l], df.Open[df.Date.size - 2 - rows * probeDist - l], df.Close[df.Date.size - 2 - rows * probeDist - l])
-
-    for i in range(df.Date.size - 2 - 10 - rows * probeDist, df.Date.size - 372 - rows * probeDist, -1):
-        if counter == 0:
-            # values as in data file
-            avgData = avg_value(df.High[i], df.Low[i], df.Open[i], df.Close[i])
-            tempData[0] = avgData  # High
-            tempData[1] = avgData  # Low
-            tempData[2] = avgData  # Open
-            tempData[3] = avgData  # Close
-        else:
-            # values of multiple days streamlined into one
-            avgData = avg_value(df.High[i], df.Low[i], df.Open[i], df.Close[i])
-            tempData[0] = max(avgData, tempData[0])  # High
-            tempData[1] = min(avgData, tempData[1])  # Low
-            tempData[3] = avgData  # Close
-
-        # after performing above operations day is added to the count
-        counter += 1
-
-        # if vector is full, start working on the next one
-        if loop == 30:
-            loop = 0
-            rows += 1
-
-        # print(loop, end=', ')
-        # print(rows)
-
-        # if data set is fully realised (enough days worth of data was compiled into it),
-        # fill vector with it
-        if (tjIndex != numOfValues) and (counter == timeJump[tjIndex]) and (rows < vectors.shape[0]):
-            for k in range(10 + loop * numOfValues, 10 + loop * numOfValues + numOfValues, 1):
-                vectors[rows][k] = tempData[(k - 10) % numOfValues]
-            loop += 1
-            counter = 0
-            # after injecting data into vector, add to the count of data sets with the same
-            # timeJump in this vector
-            age += 1
-            # if enough same days length data sets has been used, expand the scope (move timeJump
-            # by 1 step)
-            if age == (10 - tjIndex):
-                tjIndex += 1
-                age = 0
-    tjIndex = 0
-    age = 0
-    counter = 0
-    loop = 0
-
-#print(vectors)
+#vectors, y_values = get_vectorS(dateStart, dateEnd, noRows) #returns noRows = 8 vectors
