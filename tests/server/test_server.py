d07c98c580957bdae2c75ba2687259f4169e250c
vis4rd <alagaesia.vrael@gmail.com>
2023-04-01 01:01:18 +0200

    Add moar tests

--- tests/server/test_server.py
+++ tests/server/test_server.py
@@ -8,6 +8,7 @@ from flask.testing import FlaskClient
 from werkzeug.serving import BaseWSGIServer
 
 from src.server import Server
+from src.server.auth import TokenService
 from src.server.database import DatabaseHandler, DatabaseProvider, Message
 
 
@@ -140,3 +141,133 @@ class Test_Server:
                     json={"email": "legit_email@gmail.com", "password": "wrong_password"},
                 )
                 assert response.status_code == 403
+
+        class Test_MeEndpoint:
+            @pytest.fixture(autouse=True)
+            def prepare_tests(self, client: FlaskClient) -> None:
+                self.register_path: str = "api/v1/auth/register"
+                self.login_path: str = "api/v1/auth/login"
+                self.url_path: str = "api/v1/auth/me"
+                self.client: FlaskClient = client
+
+            @pytest.fixture(name="token")
+            def fixture_register_and_login(self) -> str:
+                self.client.post(
+                    self.register_path,
+                    json={"email": "legit_email@gmail.com", "password": "thelegend27"},
+                )
+                login_response = self.client.post(
+                    self.login_path,
+                    json={"email": "legit_email@gmail.com", "password": "thelegend27"},
+                )
+                return login_response.get_json()["auth_token"]
+
+            def test_send_405_on_invalid_method(self) -> None:
+                response = self.client.post(self.url_path)
+                assert response.status_code == 405
+
+            @pytest.mark.skip("Currently returns 401 due to internal error on token validation")
+            def test_send_500_on_internal_error(
+                self, token, failing_database_handler: Mock
+            ) -> None:
+                response = self.client.get(
+                    self.url_path,
+                    headers={
+                        "x-access-token": token,
+                    },
+                )
+                assert response.status_code == 500
+
+            def test_send_401_when_unauthorized_no_token(self) -> None:
+                response = self.client.get(self.url_path)
+                assert response.status_code == 401
+
+            @pytest.mark.skip("Not possible to achieve: token = registered, no token = 401")
+            def test_send_401_when_unauthorized_user_not_registered(
+                self, token, failing_database_handler: Mock
+            ) -> None:
+                response = self.client.get(
+                    self.url_path,
+                    headers={
+                        "x-access-token": token,
+                    },
+                )
+                assert response.status_code == 401
+
+            def test_send_200_on_success(self, token) -> None:
+                # register, login, retrieve token and get to me endpoint
+                response = self.client.get(
+                    self.url_path,
+                    headers={
+                        "x-access-token": token,
+                    },
+                )
+                assert response.status_code == 200
+
+        class Test_LogoutEndpoint:
+            @pytest.fixture(autouse=True)
+            def prepare_tests(self, client: FlaskClient) -> None:
+                self.register_path: str = "api/v1/auth/register"
+                self.login_path: str = "api/v1/auth/login"
+                self.url_path: str = "api/v1/auth/logout"
+                self.client: FlaskClient = client
+
+            @pytest.fixture(name="token")
+            def fixture_register_and_login(self) -> str:
+                self.client.post(
+                    self.register_path,
+                    json={"email": "legit_email@gmail.com", "password": "thelegend27"},
+                )
+                login_response = self.client.post(
+                    self.login_path,
+                    json={"email": "legit_email@gmail.com", "password": "thelegend27"},
+                )
+                return login_response.get_json()["auth_token"]
+
+            @pytest.fixture(name="token_already_revoked")
+            def mock_is_token_revoked(self, monkeypatch) -> Mock:
+                mock = Mock(return_value=True)
+                monkeypatch.setattr(TokenService, "is_token_revoked", mock)
+                return mock
+
+            @pytest.fixture(name="token_revoke_failure")
+            def mock_revoke_token(self, monkeypatch) -> Mock:
+                mock = Mock(return_value=Message.UNKNOWN_ERROR)
+                monkeypatch.setattr(TokenService, "revoke_token", mock)
+                return mock
+
+            def test_send_401_when_unauthorized_no_token(self) -> None:
+                # logout without token
+                response = self.client.post(self.url_path)
+                assert response.status_code == 401
+
+            def test_send_401_when_unauthorized_token_revoked(
+                self, token, token_already_revoked
+            ) -> None:
+                # logout with revoked token
+                response = self.client.post(
+                    self.url_path,
+                    headers={
+                        "x-access-token": token,
+                    },
+                )
+                assert response.status_code == 401
+
+            def test_send_500_on_revoke_failure(self, token, token_revoke_failure) -> None:
+                # logout halted due to token revoke fail
+                response = self.client.post(
+                    self.url_path,
+                    headers={
+                        "x-access-token": token,
+                    },
+                )
+                assert response.status_code == 500
+
+            def test_send_201_on_success(self, token) -> None:
+                response = self.client.post(
+                    self.url_path,
+                    headers={
+                        "x-access-token": token,
+                    },
+                )
+                assert response.status_code == 201
