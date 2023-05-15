4452f694a856c9365672667f736ccb4b84f94114
Rafał Walkowiak <rwalkowiak@student.agh.edu.pl>
2023-05-15 18:44:09 +0200

    Revert "[CTB-47] Creating and training model"
    This reverts commit 3d94b02c5c0ab43d71dcb9111c1110e0451e082a.

--- src/server/constants.py
+++ src/server/constants.py
@@ -1,8 +1,6 @@
 import os
 from dataclasses import dataclass
 
-from psycopg.abc import Query
-
 
 @dataclass(frozen=True)
 class PATHS:
@@ -13,7 +11,6 @@ class PATHS:
 
     RESOURCES: str = APPLICATION_ROOT_PATH + "res/"
     DATASETS: str = RESOURCES + "datasets/"
-    VALIDATION_SCHEMAS: str = RESOURCES + "schemas/"
 
     DATABASE_SCHEMA: str = RESOURCES + "schema.sql"
     DATABASE_DEFAULT_DUMP: str = RESOURCES + "dump.sql"
@@ -35,40 +32,29 @@ class CONSTANTS:
 class QUERIES:
     """All SQL queries used in the project."""
 
-    SELECT_USER_UUID: Query = "SELECT uuid FROM users WHERE uuid=%s"
-    SELECT_USER_EMAIL: Query = "SELECT email FROM users WHERE email=%s"
-    SELECT_USER_EMAIL_BY_UUID: Query = "SELECT email FROM users WHERE uuid=%s"
-    SELECT_USER_LOGIN_DATA_BY_EMAIL: Query = (
+    SELECT_USER_UUID: str = "SELECT uuid FROM users WHERE uuid=%s"
+    SELECT_USER_EMAIL: str = "SELECT email FROM users WHERE email=%s"
+    SELECT_USER_EMAIL_BY_UUID: str = "SELECT email FROM users WHERE uuid=%s"
+    SELECT_USER_LOGIN_DATA_BY_EMAIL: str = (
         "SELECT uuid, email, password_hash FROM users WHERE email=%s"
     )
-    SELECT_USER_DATA_BY_UUID: Query = (
-        "SELECT email, wallet_usd, wallet_btc FROM users WHERE uuid=%s"
-    )
-    INSERT_USER: Query = "INSERT INTO users(uuid, email, password_hash) VALUES (%s, %s, %s)"
+    SELECT_USER_DATA_BY_UUID: str = "SELECT email, wallet_usd, wallet_btc FROM users WHERE uuid=%s"
+    INSERT_USER: str = "INSERT INTO users(uuid, email, password_hash) VALUES (%s, %s, %s)"
 
-    SELECT_REVOKED_TOKEN: Query = "SELECT token FROM revoked_tokens WHERE token=%s AND expiry > %s"
-    INSERT_REVOKED_TOKEN: Query = "INSERT INTO revoked_tokens (token, expiry) VALUES (%s, %s)"
+    SELECT_REVOKED_TOKEN: str = "SELECT token FROM revoked_tokens WHERE token=%s AND expiry > %s"
+    INSERT_REVOKED_TOKEN: str = "INSERT INTO revoked_tokens (token, expiry) VALUES (%s, %s)"
 
-    SELECT_CHART: Query = """SELECT date, value FROM exchange_rate_history
+    SELECT_CHART: str = """SELECT date, value FROM exchange_rate_history
                         WHERE date BETWEEN %s and %s
                         ORDER BY date"""
-    SELECT_CHART_AGGREGATED: Query = """SELECT (DATE_PART('day', date - %s) / %s)::INT as period_number,
+    SELECT_CHART_AGGREGATED: str = """SELECT (DATE_PART('day', date - %s) / %s)::INT as period_number,
                                         MIN(date)::DATE, AVG(value), MIN(value), MAX(value)
                                         FROM exchange_rate_history
                                         WHERE date BETWEEN %s AND %s
                                         GROUP BY period_number
                                         ORDER BY period_number"""
 
-    WALLET_DEPOSIT: Query = "UPDATE users SET wallet_usd = wallet_usd + %s WHERE uuid=%s"
-    WALLET_WITHDRAW: Query = "UPDATE users SET wallet_usd = wallet_usd - %s WHERE uuid=%s"
-
-    SELECT_LATEST_STOCK_PRICE: Query = """SELECT value FROM exchange_rate_history
-                        ORDER BY date DESC
-                        LIMIT 1"""
-    WALLET_BUY_ADD_BTC: Query = "UPDATE users SET wallet_btc = wallet_btc + %s WHERE uuid=%s"
-    WALLET_BUY_SUBTRACT_USD: Query = "UPDATE users SET wallet_usd = wallet_usd - %s WHERE uuid=%s"
-
-    SELECT_ALL_RATE_HISTORY: Query = "SELECT date, value FROM exchange_rate_history"
-    SELECT_ALL_RATE_HISTORY_DESC: Query = (
+    SELECT_ALL_RATE_HISTORY: str = "SELECT date, value FROM exchange_rate_history"
+    SELECT_ALL_RATE_HISTORY_DESC : str = (
         "SELECT date, value FROM exchange_rate_history ORDER BY date DESC LIMIT %s"
     )
