d14472abf69b4661200041017d41c5667828eb1e
PiotrDeda <piotrdeda.pl@gmail.com>
2023-05-27 15:20:24 +0200

    Fixed naming

--- src/server/wallet/wallet_transaction.py
+++ src/server/wallet/wallet_transaction.py
@@ -4,14 +4,14 @@ class WalletTransaction:
     def __init__(
         self,
         timestamp: str,
-        type: str,
+        transaction_type: str,
         amount_usd: float,
         amount_btc: float,
         total_usd_after_transaction: float,
         total_btc_after_transaction: float,
     ):
         self.timestamp: str = timestamp
-        self.type: str = type
+        self.transaction_type: str = transaction_type
         self.amount_usd: float = amount_usd
         self.amount_btc: float = amount_btc
         self.total_usd_after_transaction: float = total_usd_after_transaction
